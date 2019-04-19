2597818478fa13aeb0c4c5e4d1cbaf91a276e343
Joakim Strandberg <joast388@student.liu.se>
2019-04-19 13:13:18 +0200

    Deployment fixes

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8080;
+
+//Static file declaration
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = '../frontend/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/../frontend/public/index.html'));
+})
+
+//start server
+app.listen(port, (req, res) => {
+  console.log( `server listening on port: ${port}`);
+})
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import '../style/App.css';
 
@@ -8,18 +8,38 @@ import Navbar from "./Navbar";
 
 /*
 TODO: Fix proper datafetching on server side.
+TODO: add randomize button
+TODO: add transistion button
 */
-const App = () => (
-  <div>
-    <div>
-      <Navbar/>
-    </div>
-    <Switch>
-      <Route path="/faces" exact component={FacesContainer} />
-      <Route path="/mnist" exact component={MnistContainer} />
-      <Route path="/" exact component={MnistContainer} />
-    </Switch>
-  </div>
-)
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+    this.connecToServer = this.connecToServer.bind(this);
+  }
+  connecToServer() {
+    fetch('/');
+  }
+
+  componentDidMount() {
+    this.connecToServer();
+  }
+
+  render() {
+    return(
+      <div>
+        <div>
+          <Navbar/>
+        </div>
+        <Switch>
+          <Route path="/faces" exact component={FacesContainer} />
+          <Route path="/mnist" exact component={MnistContainer} />
+          <Route path="/" exact component={MnistContainer} />
+        </Switch>
+      </div>
+    )
+  }
+}
 
 export default App;
