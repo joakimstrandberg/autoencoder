e541e1d635e36f43331389dd3421a1117f7413f9
Joakim Strandberg <joast388@student.liu.se>
2019-04-20 22:00:57 +0200

    unignored build

---
+++ webserver/frontend/build/precache-manifest.f628713e6c5799bcf610a12e7fdc58ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ae80fcee5a11e286fb8",
+    "url": "/static/js/main.52cd1c6a.chunk.js"
+  },
+  {
+    "revision": "aa932390f1d2acf5a40d",
+    "url": "/static/js/2.7cc36bb9.chunk.js"
+  },
+  {
+    "revision": "7ae80fcee5a11e286fb8",
+    "url": "/static/css/main.d60c7789.chunk.css"
+  },
+  {
+    "revision": "aa932390f1d2acf5a40d",
+    "url": "/static/css/2.3e2f157a.chunk.css"
+  },
+  {
+    "revision": "36360a57a308861e0ab67953289af269",
+    "url": "/index.html"
+  }
+];
