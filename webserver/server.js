636412c2be892207fe66d5fdf9e9c71373f6c0c0
Joakim Strandberg <joast388@student.liu.se>
2019-04-21 09:57:20 +0200

    try fix problem deploying app...

--- webserver/server.js
+++ webserver/server.js
@@ -1,25 +1,19 @@
 const express = require('express');
-const app = express();
+//const bodyParser = require('body-parser');
 const path = require('path');
-const port = process.env.PORT || 8080;
-
-//Static file declaration
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+const app = express();
+const port = process.env.PORT || 5000;
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 //production mode
-if(process.env.NODE_ENV === 'production') {
+console.log(process.env);
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
   app.use(express.static(path.join(__dirname, 'frontend/build')));
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'frontend/build/index.html'));
-  })
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  });
 }
-//build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/frontend/public/index.html'));
-})
-
-//start server
-app.listen(port, (req, res) => {
-  console.log( `server listening on port: ${port}`);
-})
+app.listen(port, () => console.log(`Listening on port ${port}`));
