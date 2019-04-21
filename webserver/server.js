b527caac63d0677765910ff1648b5255c8ba3daf
Joakim Strandberg <joast388@student.liu.se>
2019-04-21 16:13:54 +0200

    stuff

--- webserver/server.js
+++ webserver/server.js
@@ -17,10 +17,10 @@ app.post('/api/world', (req, res) => {
 });
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
   });
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
